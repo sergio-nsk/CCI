9d7c52f2eeb17108c0cfa81c23c9ea7e1bd1bb0d
sergio-nsk <sergio-nsk@users.noreply.github.com>
2017-04-23 13:08:36 +0700

    Amazon interview: Goods categories

--- Real Interviews/3.cpp
+++ Real Interviews/3.cpp
@@ -1,7 +1,17 @@
+// Each goods has a category, that is a subcategory of a parent category.
+// For example "Laptops" and "PCs" are in "Computers" and "TVs" and "Computers" are in "Electronics".
+// Draw categories tree like below:
+// Electronics
+//     Computers
+//         Laptops
+//         PCs
+//     TVs
+
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <typeinfo>
+#include <set>
+#include <algorithm>
 #include <iostream>
 
 struct Edge
@@ -10,15 +20,39 @@ struct Edge
     std::string child;
 };
 
+void printSubtree(const std::string &root, const std::unordered_multimap<std::string, std::string> &edges, size_t indent)
+{
+    std::vector<std::string> childs;
+    std::cout << std::string(indent, ' ') << root << "\n";
+
+    auto ranges = edges.equal_range(root);
+    std::transform(ranges.first, ranges.second, std::back_inserter(childs), [](auto c){return c.second;});
+    std::sort(childs.begin(), childs.end());
+
+    for (const auto &child : childs)
+        printSubtree(child, edges, indent + 4);
+}
+
 int main()
 {
-    std::vector<Edge> edges = {{"Electronics", "Computers"}, {"Computers", "Laptops"}, {"Electronics", "Playstations"}, {"Electronics", "TV"}};
+    std::vector<Edge> edges = {{"Electronics", "Computers"}, {"Computers", "Laptops"}, {"Computers", "PCs"}, {"Electronics", "Playstations"}, {"Electronics", "TVs"},
+        {"Kitchen-wares", "Knifes"}, {"Kitchen-wares", "Wares"}, {"Wares", "Glasses"}east.net, {"Wares", "Plates"}};
+
     std::unordered_multimap<std::string, std::string> hashEdges;
+    std::set<std::string> parents;
+    std::set<std::string> childs;
+    std::vector<std::string> roots;
 
     for (auto &e : edges)
+    {
         hashEdges.emplace(e.parent, e.child);
+        parents.emplace(e.parent);
+        childs.emplace(e.child);
+    }
     if (hashEdges.empty())
         return 0;
 
-    auto root = edges[0].parent;
+    std::set_difference(parents.begin(), parents.end(), childs.begin(), childs.end(), std::back_inserter(roots));
+    for (const auto &root : roots)
+        printSubtree(root, hashEdges, 0);
 }
