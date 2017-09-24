98ff4078099d249855c7d73f0edab3e19ca5dbc9
sergio-nsk <sergio-nsk@users.noreply.github.com>
2017-09-24 17:07:07 +0700

    Real interview: Inner product of two compressed vectors - naive and optimal solutions.

--- Real Interviews/11.cpp
+++ Real Interviews/11.cpp
@@ -11,6 +11,55 @@
 
 typedef std::pair<int, int> Chunk;
 
+// Naive
+class Adapter
+{
+public:
+    Adapter(const std::vector<Chunk> &vec) : _vec(vec)
+    {
+        reset();
+    }
+
+    void reset()
+    {
+        _i = _vec.begin();
+        _cnt = _i != _vec.end() ?  _i->second : 0;
+
+    }
+
+    std::pair<bool, int> get()
+    {
+        while (_i != _vec.end() && _cnt == 0)
+        {
+            ++_i;
+            _cnt = _i->second;
+        }
+        if (_cnt != 0)
+        {
+            --_cnt;
+            return std::make_pair(true, _i->first);
+        }
+        return std::make_pair(false, 0);
+    }
+
+  private:
+    const std::vector<Chunk> &_vec;
+    std::vector<Chunk>::const_iterator _i;
+    int _cnt;
+};
+
+int innerProductNaive(const std::vector<Chunk> &vec1, const std::vector<Chunk> &vec2)
+{
+    int product = 0;
+    Adapter a1(vec1);
+    Adapter a2(vec2);
+    std::pair<bool, int> v1;
+    std::pair<bool, int> v2;
+    while ((v1 = a1.get()).first && (v2 = a2.get()).first)
+        product += v1.second * v2.second;
+    return product;
+}
+
 // Optimal
 int innerProduct(const std::vector<Chunk> &vec1, const std::vector<Chunk> &vec2)
 {
@@ -47,6 +96,12 @@ int innerProduct(const std::vector<Chunk> &vec1, const std::vector<Chunk> &vec2)
 
 int main()
 {
-    auto product = innerProduct({{1, 5}, {2, 3}}, {{2, 4}, {3, 2}, {4, 5}});
+    std::vector<Chunk> vec1 = {{1, 5}, {2, 3}};
+    std::vector<Chunk> vec2 = {{2, 4}, {3, 2}, {4, 5}};
+
+    auto product = innerProductNaive(vec1, vec2);
+    std::cout << product << std::endl;
+
+    product = innerProduct(vec1, vec2);
     std::cout << product << std::endl;
 }
