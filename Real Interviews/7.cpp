da20a67049a643e727cdc8130435981ecf684305
sergio-nsk <sergio-nsk@users.noreply.github.com>
2017-10-01 15:40:58 +0700

    Real interview: Compress long string.

---
+++ Real Interviews/7.cpp
@@ -0,0 +1,49 @@
+// Compress long string.
+// Example:  "AAAAAAAAAAAAAAABBC" -> "A15B2C"
+
+#include <string>
+#include <iostream>
+
+std::string::iterator Replace(std::string &str, std::string::iterator from, const std::string &substr)
+{
+    str.replace(from, from + substr.length(), substr);
+    return from + substr.length();
+}
+
+std::string &Compress(std::string &str)
+{
+    if (str.size() <= 1)
+        return str;
+
+    auto dst = str.begin();
+    auto src = dst + 1;
+    auto c = *dst;
+    std::size_t cnt = 1;
+
+    while (src != str.end())
+    {
+        if (*src == c)
+        {
+            ++src;
+            ++cnt;
+        }
+        else
+        {
+            *dst++ = c;
+            dst = Replace(str, dst, std::to_string(cnt));
+            c = *src++;
+            cnt = 1;
+        }
+    }
+    *dst++ = c;
+    if (cnt > 1)
+        dst = Replace(str, dst, std::to_string(cnt));
+    str.erase(dst, str.end());
+    return str;
+}
+
+int main()
+{
+    std::string str = "AAAAAAAAAAAAAAABBC";
+    std::cout << Compress(str) << std::endl;
+}
