3b90e0538e7f54e9696a68c62d6238f5450e934f
sergio-nsk <sergio-nsk@users.noreply.github.com>
2018-04-08 19:16:24 +0700

    clang-format

--- 1.Arrays&Strings/1.9.c
+++ 1.Arrays&Strings/1.9.c
@@ -1,36 +1,36 @@
 /*
- String Rotation: Assumeyou have a method isSubstringwhich checks if one word is a substring
- of another. Given two strings, sl and s2, write code to check if s2 is a rotation of sl using only one
- call to isSubstring (e.g., "waterbottle" is a rotation of "erbottlewat").
+ String Rotation: Assumeyou have a method isSubstringwhich checks if one word is
+ a substring of another. Given two strings, sl and s2, write code to check if s2
+ is a rotation of sl using only one call to isSubstring (e.g., "waterbottle" is
+ a rotation of "erbottlewat").
  */
 
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
-int checkRotation(const char *one, const char *two)
-{
-    const int len = strlen(one);
-    if (len != strlen(two))
-        return 0;
-
-    char oneone[2 * len + 1];
-
-    strcpy(oneone, one);
-    strcpy(oneone + len, one);
-    return strstr(oneone, two) != NULL;
-}
-
-void test(int (*func)(const char *, const char *), const char *one, const char *two)
-{
-    printf("\"%s\" %s rotated \"%s\"\n", two, func(one, two) ? "is" : "is not", one);
-}
-
-
-int main(int argc, char **argv)
-{
-    test(checkRotation, "TestRotation", "RotationTest");
-    test(checkRotation, "TestRotation", "rotationTest");
-    test(checkRotation, "TestRottation", "Rotationtest");
-    test(checkRotation, "waterbottle", "erbottlewat");
+int checkRotation(const char* one, const char* two) {
+  const int len = strlen(one);
+  if (len != strlen(two))
     return 0;
+
+  char oneone[2 * len + 1];
+
+  strcpy(oneone, one);
+  strcpy(oneone + len, one);
+  return strstr(oneone, two) != NULL;
+}
+
+void test(int (*func)(const char*, const char*),
+          const char* one,
+          const char* two) {
+  printf("\"%s\" %s rotated \"%s\"\n", two, func(one, two) ? "is" : "is not",
+         one);
+}
+
+int main(int argc, char** argv) {
+  test(checkRotation, "TestRotation", "RotationTest");
+  test(checkRotation, "TestRotation", "rotationTest");
+  test(checkRotation, "TestRottation", "Rotationtest");
+  test(checkRotation, "waterbottle", "erbottlewat");
+  return 0;
 }
--- 1.Arrays&Strings/1.9.cpp
+++ 1.Arrays&Strings/1.9.cpp
@@ -1,31 +1,31 @@
-// String Rotation: Assumeyou have a method isSubstringwhich checks if one word is a substring
-// of another. Given two strings, sl and s2, write code to check if s2 is a rotation of sl using only one
-// call to isSubstring (e.g., "waterbottle" is a rotation of "erbottlewat").
+// String Rotation: Assumeyou have a method isSubstringwhich checks if one word
+// is a substring of another. Given two strings, sl and s2, write code to check
+// if s2 is a rotation of sl using only one call to isSubstring (e.g.,
+// "waterbottle" is a rotation of "erbottlewat").
 
 #include "all.hpp"
 
-bool checkRotation(std::string &&one, std::string &&two)
-{
-    if (one.length() != two.length())
-        return false;
+bool checkRotation(std::string&& one, std::string&& two) {
+  if (one.length() != two.length())
+    return false;
 
-    one += one;
-    return one.find(two) != one.npos;
+  one += one;
+  return one.find(two) != one.npos;
 }
 
-void printResult(bool result, std::string &&one, std::string &&two)
-{
-    if (result)
-        std::cout << '"' << two << '"' << " is rotated " << '"' << one << '"'  << std::endl;
-    else
-        std::cout << '"' << two << '"' << " is not rotaded " << '"' << one << '"'  << std::endl;
+void printResult(bool result, std::string&& one, std::string&& two) {
+  if (result)
+    std::cout << '"' << two << '"' << " is rotated " << '"' << one << '"'
+              << std::endl;
+  else
+    std::cout << '"' << two << '"' << " is not rotated " << '"' << one << '"'
+              << std::endl;
 }
 
-int main()
-{
-    test(checkRotation, printResult, "TestRotation", "RotationTest");
-    test(checkRotation, printResult, "TestRotation", "rotationTest");
-    test(checkRotation, printResult, "TestRotation", "Rotationtest");
-    test(checkRotation, printResult, "waterbottle", "erbottlewat");
-    return 0;
+int main() {
+  test(checkRotation, printResult, "TestRotation", "RotationTest");
+  test(checkRotation, printResult, "TestRotation", "rotationTest");
+  test(checkRotation, printResult, "TestRotation", "Rotationtest");
+  test(checkRotation, printResult, "waterbottle", "erbottlewat");
+  return 0;
 }
