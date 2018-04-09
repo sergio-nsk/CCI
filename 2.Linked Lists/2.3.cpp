f226958b63582b847c4c01b2ddb94c5776675aed
sergio-nsk <sergio-nsk@users.noreply.github.com>
2018-04-09 22:54:40 +0700

    clang-format

--- 2.Linked Lists/2.3.cpp
+++ 2.Linked Lists/2.3.cpp
@@ -1,6 +1,6 @@
-// Delete Middle Node: Implement an algorithm to delete a node in the middle (i.e., any node but
-// the first and last node, not necessarily the exact middle) of a singly linked list, given only access to
-// that node.
+// Delete Middle Node: Implement an algorithm to delete a node in the middle
+// (i.e., any node but the first and last node, not necessarily the exact
+// middle) of a singly linked list, given only access to that node.
 // EXAMPLE
 // Input: the node c from the linked list a->b->c->d->e->f
 // Result: nothing is returned, but the new linked list looks like a->b->d->e->f
@@ -11,39 +11,35 @@ template <typename T>
 using Node = typename forward_list<T>::Node;
 
 template <typename T>
-bool deleteMiddleNode(Node<T> *node)
-{
-    Node<T> *next = node->next;
-    node->value = next->value;
-    node->next = next->next;
-    delete next;
-    return true;
+bool deleteMiddleNode(Node<T>* node) {
+  Node<T>* next = node->next;
+  node->value = next->value;
+  node->next = next->next;
+  delete next;
+  return true;
 }
 
 template <typename T>
-void printList(bool result, const forward_list<T> &list)
-{
-    const char *sep = "";
-    for (auto &c : list)
-    {
-        std::cout << sep << c;
-        sep = ", ";
-    }
+void printList(bool result, const forward_list<T>& list) {
+  const char* sep = "";
+  for (auto& c : list) {
+    std::cout << sep << c;
+    sep = ", ";
+  }
 }
 
-int main()
-{
-    forward_list<int> list({0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
-    printList<int>(true, list);
-    std::cout << " --> ";
+int main() {
+  forward_list<int> list({0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+  printList<int>(true, list);
+  std::cout << " --> ";
 
-    auto detached = list.detach();
-    auto node = detached->next->next->next->next->next;
-    deleteMiddleNode<int>(node); // i becomes invalid
+  auto detached = list.detach();
+  auto node = detached->next->next->next->next->next;
+  deleteMiddleNode<int>(node);  // i becomes invalid
 
-    list.attach(detached);
-    printList<int>(true, list);
-    std::cout << std::endl;
+  list.attach(detached);
+  printList<int>(true, list);
+  std::cout << std::endl;
 
-    return 0;
+  return 0;
 }
--- 2.Linked Lists/2.4.cpp
+++ 2.Linked Lists/2.4.cpp
@@ -1,7 +1,9 @@
-// Partition: Write code to partition a linked list around a value x, such that all nodes less than x come
-// before all nodes greater than or equal to x. If x is contained within the list, the values of x only need
-// to be after the elements less than x (see below). The partition element x can appear anywhere in the
-// "right partition"; it does not need to appear between the left and right partitions.
+// Partition: Write code to partition a linked list around a value x, such that
+// all nodes less than x come before all nodes greater than or equal to x. If x
+// is contained within the list, the values of x only need to be after the
+// elements less than x (see below). The partition element x can appear anywhere
+// in the "right partition"; it does not need to appear between the left and
+// right partitions.
 // EXAMPLE
 // Input:
 //  3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition= 5]
@@ -14,77 +16,70 @@ template <typename T>
 using Node = typename forward_list<T>::Node;
 
 template <typename T>
-void swap(T &l, T &r)
-{
-    T tmp = l;
-    l = r;
-    r = tmp;
+void swap(T& l, T& r) {
+  T tmp = l;
+  l = r;
+  r = tmp;
 }
 
 template <typename T>
-bool partition(Node<T> *head, T x)
-{
-    Node<T> *current = head;
-    Node<T> *next = head;
+bool partition(Node<T>* head, T x) {
+  Node<T>* current = head;
+  Node<T>* next = head;
 
-    while (next)
-    {
-        if (next->value < x)
-        {
-            if (current != next)
-                swap(current->value, next->value);
-            current = current->next;
-        }
-        next = next->next;
+  while (next) {
+    if (next->value < x) {
+      if (current != next)
+        swap(current->value, next->value);
+      current = current->next;
     }
-    return true;
+    next = next->next;
+  }
+  return true;
 }
 
 template <typename T>
-void printList(bool result, const forward_list<T> &list)
-{
-    const char *sep = "";
-    for (auto &c : list)
-    {
-        std::cout << sep << c;
-        sep = ", ";
-    }
+void printList(bool result, const forward_list<T>& list) {
+  const char* sep = "";
+  for (auto& c : list) {
+    std::cout << sep << c;
+    sep = ", ";
+  }
 }
 
-int main()
-{
-    forward_list<int> list({3, 7, 2, 8, 1, 9, 0, 10, 5, 5, 4});
-    printList<int>(true, list);
-    std::cout << " --> ";
+int main() {
+  forward_list<int> list({3, 7, 2, 8, 1, 9, 0, 10, 5, 5, 4});
+  printList<int>(true, list);
+  std::cout << " --> ";
 
-    auto detached = list.detach();
-    partition(detached, 5);
-    list.attach(detached);
+  auto detached = list.detach();
+  partition(detached, 5);
+  list.attach(detached);
 
-    printList<int>(true, list);
-    std::cout << std::endl;
+  printList<int>(true, list);
+  std::cout << std::endl;
 
-    list = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-    printList<int>(true, list);
-    std::cout << " --> ";
+  list = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+  printList<int>(true, list);
+  std::cout << " --> ";
 
-    detached = list.detach();
-    partition(detached, 5);
-    list.attach(detached);
+  detached = list.detach();
+  partition(detached, 5);
+  list.attach(detached);
 
-    printList<int>(true, list);
-    std::cout << std::endl;
+  printList<int>(true, list);
+  std::cout << std::endl;
 
-    list = {3, 5, 8, 5, 10, 2, 1};
-    printList<int>(true, list);
-    std::cout << " --> ";
+  list = {3, 5, 8, 5, 10, 2, 1};
+  printList<int>(true, list);
+  std::cout << " --> ";
 
-    detached = list.detach();
-    partition(detached, 5);
-    list.attach(detached);
+  detached = list.detach();
+  partition(detached, 5);
+  list.attach(detached);
 
-    printList<int>(true, list);
-    std::cout << std::endl;
+  printList<int>(true, list);
+  std::cout << std::endl;
 
-    return 0;
+  return 0;
 }
