99983e9eb4a489d59003025f5e897e0086e50d20
sergio-nsk <sergio-nsk@users.noreply.github.com>
2018-10-21 18:22:06 +0700

    clang-format

--- QuickSort/qsort.c
+++ QuickSort/qsort.c
@@ -2,44 +2,41 @@
 
 static void quickSort(int array[], int left, int right);
 
-void qSort(int array[], int size)
-{
-    quickSort(array, 0, size - 1);
+void qSort(int array[], int size) {
+  quickSort(array, 0, size - 1);
 }
 
-static void quickSort(int array[], int left, int right)
-{
-    int i = left;
-    int j = right;
-    int pivot = array[(left + right) / 2];
-    int tmp;
+static void quickSort(int array[], int left, int right) {
+  int i = left;
+  int j = right;
+  int pivot = array[(left + right) / 2];
+  int tmp;
 
-    while (i <= j)
-    {
-        while (array[i] < pivot) ++i;
-        while (array[j] > pivot) --j;
-        if (i <= j)
-        {
-            tmp = array[i];
-            array[i++] = array[j];
-            array[j--] = tmp;
-        }
+  while (i < j) {
+    while (array[i] < pivot)
+      ++i;
+    while (array[j] > pivot)
+      --j;
+    if (i <= j) {
+      tmp = array[i];
+      array[i++] = array[j];
+      array[j--] = tmp;
     }
-    if (left < j)
-        quickSort(array, left, j);
-    if (i < right)
-        quickSort(array, i, right);
+  }
+  if (left < j)
+    quickSort(array, left, j);
+  if (i < right)
+    quickSort(array, i, right);
 }
 
-int main(int argc, char **argv)
-{
-    int array[] = {3, 7, 2, 8, 1, 9, 0, 10, 5, 5, 4};
-    int size = sizeof(array) / sizeof(array[0]);
-    int i = 0;
+int main(int argc, char** argv) {
+  int array[] = {3, 7, 2, 8, 1, 9, 0, 10, 5, 5, 4};
+  int size = sizeof(array) / sizeof(array[0]);
+  int i = 0;
 
-    qSort(array, size);
-    for (i = 0; i < size; ++i)
-        printf("%d ", array[i]);
-    printf("\n");
-    return 0;
+  qSort(array, size);
+  for (i = 0; i < size; ++i)
+    printf("%d ", array[i]);
+  printf("\n");
+  return 0;
 }
--- QuickSort/qsort.cpp
+++ QuickSort/qsort.cpp
@@ -1,26 +1,27 @@
 #include <algorithm>
+#include <array>
 #include <iostream>
 
 template <typename ForwardIterator>
-void qsort(ForwardIterator first, ForwardIterator last)
-{
-    if (first == last)
-        return;
+void qsort(ForwardIterator first, ForwardIterator last) {
+  if (first == last)
+    return;
 
-    auto pivot = *std::next(first, std::distance(first, last) / 2);
-    ForwardIterator mid1 = std::partition(first, last, [pivot](const decltype(pivot) &v) { return v < pivot; });
-    ForwardIterator mid2 = std::partition(mid1, last, [pivot](const decltype(pivot) &v) { return pivot >= v; });
+  auto pivot = *std::next(first, std::distance(first, last) / 2);
+  ForwardIterator mid1 = std::partition(
+      first, last, [pivot](const decltype(pivot)& v) { return v < pivot; });
+  ForwardIterator mid2 = std::partition(
+      mid1, last, [pivot](const decltype(pivot)& v) { return pivot == v; });
 
-    // Values between mid1 and mid2 are equal.
-    qsort(first, mid1);
-    qsort(mid2, last);
+  // Values between mid1 and mid2 are equal.
+  qsort(first, mid1);
+  qsort(mid2, last);
 }
 
-int main()
-{
-    std::array<int, 11> array = {3, 7, 2, 8, 1, 9, 0, 10, 5, 5, 4};
-    qsort(array.begin(), array.end());
-    for (auto v : array)
-        std::cout << v << ' ';
-    std::cout << std::endl;
+int main() {
+  std::array<int, 11> array = {3, 7, 2, 8, 1, 9, 0, 10, 5, 5, 4};
+  qsort(array.begin(), array.end());
+  for (auto v : array)
+    std::cout << v << ' ';
+  std::cout << std::endl;
 }
