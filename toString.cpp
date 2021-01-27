5b105f1c9e08e67abe8d15783cc5e4e2deb9fccc
Sergey Markelov <sergey@solidstatenetworks.com>
2021-01-26 19:38:34 -0700

    Markov's normal div algorithm

--- toString.cpp
+++ toString.cpp
@@ -14,7 +14,7 @@ template<typename T>
 constexpr bool has_std_to_string = std::experimental::is_detected<std_to_string_expression, T>;
 
 
-// 2- detecting if to_string is valid on T
+// 2 - detecting if to_string is valid on T
 
 template<typename T>
 using to_string_expression = decltype(to_string(std::declval<T>()));
@@ -23,7 +23,7 @@ template<typename T>
 constexpr bool has_to_string = std::experimental::is_detected<to_string_expression, T>;
 
 
-// 3- detecting if T can be sent to an ostringstream
+// 3 - detecting if T can be sent to an ostringstream
 
 template<typename T>
 using ostringstream_expression = decltype(std::declval<std::ostringstream&>() << std::declval<T>());
@@ -31,21 +31,21 @@ using ostringstream_expression = decltype(std::declval<std::ostringstream&>() <<
 template<typename T>
 constexpr bool has_ostringstream = std::experimental::is_detected<ostringstream_expression, T>;
 
-// 1-  std::to_string is valid on T
+// 1 -  std::to_string is valid on T
 template<typename T, typename std::enable_if<has_std_to_string<T>, int>::type = 0>
 std::string toString(T const& t)
 {
     return std::to_string(t);
 }
 
-// 2-  std::to_string is not valid on T, but to_string is
+// 2 -  std::to_string is not valid on T, but to_string is
 template<typename T, typename std::enable_if<!has_std_to_string<T> && has_to_string<T>, int>::type = 0>
 std::string toString(T const& t)
 {
     return to_string(t);
 }
 
-// 3-  neither std::string nor to_string work on T, let's stream it then
+// 3 -  neither std::string nor to_string work on T, let's stream it then
 template<typename T, typename std::enable_if<!has_std_to_string<T> && !has_to_string<T> && has_ostringstream<T>, int>::type = 0>
 std::string toString(T const& t)
 {
