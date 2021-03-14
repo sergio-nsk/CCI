8267906a729821ea07db38510a539e04454705bd
Sergey <sergio-nsk@users.noreply.github.com>
2021-03-13 17:34:20 -0700

    Fix fibonacci (integer overflow) and 8.9 (string buffer overflow)

--- 8.Recursuion&Dynamic Programming/8.5.cpp
+++ 8.Recursuion&Dynamic Programming/8.5.cpp
@@ -1,52 +1,46 @@
-// Recursive Multiply: Write a recursive function to multiply two positive integers without using
-// the * operator (or / operator). You can use addition, subtraction, and bit shifting, but you should
-// minimize the number of those operations.
+// Recursive Multiply: Write a recursive function to multiply two positive
+// integers without using the * operator (or / operator). You can use addition,
+// subtraction, and bit shifting, but you should minimize the number of those
+// operations.
 
 #include <iostream>
 
-class Product
-{
-    static int counter;
+class Product {
+  static int counter;
 
-public:
-    /* This is an algorithm called the peasant algorithm.
-     * https://en.wikipedia.org/wiki/Multiplication_algorithm#Peasant_or_binary_multiplication
-     */
-    static int minProduct(int a, int b)
-    {
-        if (a < b)
-            return minProduct(b, a);
-        int value = 0;
-        while (a > 0)
-        {
-            ++counter;
-            if (a % 2 == 1)
-                value += b;
-            a >>= 1;
-            b <<= 1;
-        }
-        return value;
+ public:
+  /* This is an algorithm called the peasant algorithm.
+   * https://en.wikipedia.org/wiki/Multiplication_algorithm#Peasant_or_binary_multiplication
+   */
+  static int minProduct(int a, int b) {
+    if (a < b)
+      return minProduct(b, a);
+    int value = 0;
+    while (a > 0) {
+      ++counter;
+      if (a % 2 == 1)
+        value += b;
+      a >>= 1;
+      b <<= 1;
     }
+    return value;
+  }
 
-    static void main()
-    {
-        for (int i = 0; i < 1000; i++)
-        {
-            for (int j = 0; j < 1000; j++)
-            {
-                int prod1 = minProduct(i, j);
-                int prod2 = i * j;
-                if (prod1 != prod2)
-                    std::cout << "ERROR: " << i << " * " << j << " = " << prod2 << ", not " << prod1 << std::endl;
-            }
-        }
+  static void main() {
+    for (int i = 0; i < 1000; i++) {
+      for (int j = 0; j < 1000; j++) {
+        int prod1 = minProduct(i, j);
+        int prod2 = i * j;
+        if (prod1 != prod2)
+          std::cout << "ERROR: " << i << " * " << j << " = " << prod2
+                    << ", not " << prod1 << std::endl;
+      }
     }
-
+  }
 };
 
 int Product::counter = 0;
 
-int main()
-{
-    Product::main();
+int main() {
+  Product::main();
 }
--- 8.Recursuion&Dynamic Programming/fibonacci.cpp
+++ 8.Recursuion&Dynamic Programming/fibonacci.cpp
@@ -1,37 +1,34 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 
-long fibonacci(int n, std::vector<long> &fibs);
+long long fibonacci(int n, std::vector<long long>& fibs);
 
-long fibonacci(int n)
-{
-    std::vector<long> fibs(n + 1);
-    return fibonacci(n, fibs);
+long long fibonacci(int n) {
+  std::vector<long long> fibs(n + 1);
+  return fibonacci(n, fibs);
 }
 
-long fibonacci(int n, std::vector<long> &fibs)
-{
-    if (n == 0 || n == 1)
-        return 1;
-    if (fibs[n] == 0)
-        fibs[n] = fibonacci(n - 1, fibs) + fibonacci(n - 2, fibs);
-    return fibs[n];
+long long fibonacci(int n, std::vector<long long>& fibs) {
+  if (n == 0 || n == 1)
+    return 1;
+  if (fibs[n] == 0)
+    fibs[n] = fibonacci(n - 1, fibs) + fibonacci(n - 2, fibs);
+  return fibs[n];
 }
 
-long fibonacci_loop(int n)
-{
-    if (n == 0 || n == 1)
-        return 1;
+long long fibonacci_loop(int n) {
+  if (n == 0 || n == 1)
+    return 1;
 
-    long fibs[2] = {1, 1};
-    for ( ; n > 2; --n)
-        fibs[n & 1] = fibs[0] + fibs[1];
-    return fibs[0] + fibs[1];
+  long long fibs[2] = {1, 1};
+  for (; n > 2; --n)
+    fibs[n & 1] = fibs[0] + fibs[1];
+  return fibs[0] + fibs[1];
 }
 
-int main()
-{
-    int n = 100;
-    std::cout << "fibonacci(" << n << "): " << fibonacci(n) << std::endl;
-    std::cout << "fibonacci_loop(" << n << "): " << fibonacci_loop(n) << std::endl;
+int main() {
+  int n = 100;
+  std::cout << "fibonacci(" << n << "):      " << fibonacci(n) << std::endl;
+  std::cout << "fibonacci_loop(" << n << "): " << fibonacci_loop(n)
+            << std::endl;
 }
