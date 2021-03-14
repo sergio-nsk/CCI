58e98470cea0933597d8f5b70807c948302f6248
Sergey <sergio-nsk@users.noreply.github.com>
2021-03-13 17:44:27 -0700

    main in 8.11 and clangformat

--- 8.Recursuion&Dynamic Programming/8.11.cpp
+++ 8.Recursuion&Dynamic Programming/8.11.cpp
@@ -1,25 +1,31 @@
-// Coins: Given an infinite number of quarters (25 cents), dimes (1O cents), nickels (5 cents), and
-// pennies (1 cent), write code to calculate the number of ways of representing n cents.
+// Coins: Given an infinite number of quarters (25 cents), dimes (1O cents),
+// nickels (5 cents), and pennies (1 cent), write code to calculate the number
+// of ways of representing n cents.
 
+#include <iostream>
 #include <vector>
 
-int makeChange(int amount, std::vector<int> &denoms, unsigned index)
-{
-    if (index >= denoms.size() - 1)
-        return 1; // last denom
-    int denomAmount = denoms[index];
-    int ways = 0;
-    for (int i = 0; i * denomAmount <= amount; ++i)
-    {
-        //go to next denom, assuming i coins of denomAmount
-        int amountRemaining = amount - i * denomAmount;
-        ways += makeChange(amountRemaining, denoms, index + 1);
-    }
-    return ways;
+int makeChange(int amount, std::vector<int>& denoms, unsigned index) {
+  if (index >= denoms.size() - 1)
+    return 1;  // last denom
+  int denomAmount = denoms[index];
+  int ways = 0;
+  for (int i = 0; i * denomAmount <= amount; ++i) {
+    // go to next denom, assuming i coins of denomAmount
+    int amountRemaining = amount - i * denomAmount;
+    ways += makeChange(amountRemaining, denoms, index + 1);
+  }
+  return ways;
 }
 
-int makeChange(int n)
-{
-    std::vector<int> denoms = {25, 10, 5, 1};
-    return makeChange(n, denoms, 0);
+int makeChange(int n) {
+  std::vector<int> denoms = {25, 10, 5, 1};
+  return makeChange(n, denoms, 0);
+}
+
+int main() {
+  for (int i = 0; i <= 25; ++i) {
+    std::cout << i << " cents can be changed in " << makeChange(i) << " ways"
+              << std::endl;
+  }
 }
